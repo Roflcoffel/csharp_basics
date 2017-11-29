9a93b53ce1f2c33d83611064b0ffce2deb340540
Roflcoffel <roflcoffel@gmail.com>
2017-11-29 21:11:08 +0100

    Magic Square

---
+++ MagicSquare/MagicSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// shuld try to generate a magic square.
+/// and if possible a magic square of square numbers.
+/// 
+/// Magic Square Rules:
+/// 9 Unique numbers
+/// ordered in a 3x3 grid
+/// the sum of every row, column and diagnol must equal the same number
+/// </summary>
+namespace MagicSquare {
+    class Program {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MagicSquare/MagicSquare/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSquare {
+    class Square {
+        public int[,] Grid { set; get; }
+
+        Square()
+        {
+            Grid = new int[3, 3];
+        }
+
+        //Sums all 3 col
+        public int[] SumCol()
+        {
+            return new int[3];
+        }
+
+        public int[] SumRow(int row)
+        {
+            return new int[3];
+        }
+
+        public int[] SumDia()
+        {
+            return new int[2];
+        }
+    }
+}
