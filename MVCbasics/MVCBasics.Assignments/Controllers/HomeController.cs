85f3aa12297c35de82380e275654da284c5bd81d
Roflcoffel <roflcoffel@gmail.com>
2017-12-05 10:58:03 +0100

    Rename Hell

---
+++ MVCbasics/MVCBasics.Assignments/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCbasics.Assignments.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Home / Index
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Layout and view assignment";
+            ViewBag.List = new List<string>() {"Testing","razor","in","a", "list"};
+
+
+            return View();
+        }
+
+        // Home / About
+        public ActionResult About()
+        {
+            ViewBag.Message = "Here is a short introduction page";
+
+            return View();
+        }
+
+        // Home / Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Here are my contacts";
+
+            return View();
+        }
+
+        // Home / Projects
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Here are some projects";
+            return View();
+        }
+    }
+}
